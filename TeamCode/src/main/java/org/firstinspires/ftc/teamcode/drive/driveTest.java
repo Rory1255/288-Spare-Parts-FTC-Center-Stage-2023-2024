5dbed41ea4ebe9f3b87ea48ed9a1f9e0d4284ef3
Programming <rorypeyton1255@gmail.com>
2023-09-15 10:41:21 -0500

    initial commit for roadrunner- Rory

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/driveTest.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.drive;
+
+import android.annotation.SuppressLint;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name = "Drive Test", group = "Linear Opmode")
+public class driveTest extends LinearOpMode {
+    //Variable declarations
+    private DcMotor leftFrontDriveMotor = null;
+    private DcMotor rightFrontDriveMotor = null;
+    private DcMotor leftBackDriveMotor = null;
+    private DcMotor rightBackDriveMotor = null;
+
+
+    @SuppressLint("DefaultLocale")
+    @Override
+    public void runOpMode(){
+        //Hardware Mapping
+        leftFrontDriveMotor = hardwareMap.get(DcMotor.class, "leftFrontDriveMotor");
+        rightFrontDriveMotor = hardwareMap.get(DcMotor.class, "rightFrontDriveMotor");
+        leftBackDriveMotor = hardwareMap.get(DcMotor.class, "leftBackDriveMotor");
+        rightBackDriveMotor = hardwareMap.get(DcMotor.class, "rightBackDriveMotor");
+
+        //Set motor direction
+        leftFrontDriveMotor.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontDriveMotor.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDriveMotor.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDriveMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        //Set Zero Power Behavior
+        //leftFrontDriveMotor
+        //rightFrontDriveMotor
+        //leftBackDriveMotor
+       // rightBackDriveMotor
+
+        //Telemetry(information that gets sent to drive station for quick feedback)
+    }
+}
