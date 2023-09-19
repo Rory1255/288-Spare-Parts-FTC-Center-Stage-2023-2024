0aa7b66b869f0ab208d23f5f0eb5bf34e08236f2
Programming <rorypeyton1255@gmail.com>
2023-09-19 16:53:31 -0500

    added elevator control- Rory

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/driveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/driveTest.java
@@ -1,10 +1,15 @@
 package org.firstinspires.ftc.teamcode.drive;
 
+import static java.lang.Math.max;
+import static java.lang.StrictMath.min;
+
 import android.annotation.SuppressLint;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 
 @TeleOp(name = "Drive Test", group = "Linear Opmode")
@@ -15,7 +20,12 @@ public class driveTest extends LinearOpMode {
     private DcMotor leftBackDriveMotor = null;
     private DcMotor rightBackDriveMotor = null;
 
+    private DcMotor elevatorMotor = null;
 
+    final double ELEVATOR_HEIGHT_MAX = 4157;
+    private double targetElevatorPosition = 0;
+    private final ElapsedTime runtime = new ElapsedTime();
+    final double ELEVATOR_HEIGHT_SHORT = 978;
     @SuppressLint("DefaultLocale")
     @Override
     public void runOpMode(){
@@ -25,11 +35,18 @@ public class driveTest extends LinearOpMode {
         leftBackDriveMotor = hardwareMap.get(DcMotor.class, "leftBackDriveMotor");
         rightBackDriveMotor = hardwareMap.get(DcMotor.class, "rightBackDriveMotor");
 
+
+        elevatorMotor = hardwareMap.get(DcMotor.class, "elevatorMotor");
+
         //Set motor direction
         leftFrontDriveMotor.setDirection(DcMotor.Direction.FORWARD);
         rightFrontDriveMotor.setDirection(DcMotor.Direction.REVERSE);
         leftBackDriveMotor.setDirection(DcMotor.Direction.FORWARD);
         rightBackDriveMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        elevatorMotor.setDirection(DcMotor.Direction.REVERSE);
+        elevatorMotor.setTargetPosition(0);
+        elevatorMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
         //Set Zero Power Behavior
         leftFrontDriveMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -38,5 +55,25 @@ public class driveTest extends LinearOpMode {
         rightBackDriveMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         //Telemetry(information that gets sent to drive station for quick feedback)
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        waitForStart();
+        runtime.reset();
+
+
+        //runtime until driver hits stop
+        while (opModeIsActive()) {
+
+            if (gamepad1.a) {
+                targetElevatorPosition = ELEVATOR_HEIGHT_SHORT;
+            }
+
+            targetElevatorPosition = max(0.0, targetElevatorPosition); // Cannot go below 0
+            targetElevatorPosition = min(targetElevatorPosition, ELEVATOR_HEIGHT_MAX); // Cannot go beyond max height
+
+            elevatorMotor.setTargetPosition((int) targetElevatorPosition);
+            elevatorMotor.setPower(1.0);
+
+        }
     }
 }
