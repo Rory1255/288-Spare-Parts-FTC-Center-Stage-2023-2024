22eb275c695f4e97cc6cc6a4f3a6df6d8617efc4
Programming <rorypeyton1255@gmail.com>
2023-10-18 13:04:55 -0500

    added field centric drive and tested servo control- Rory

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/driveTestForReal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/driveTestForReal.java
@@ -1,14 +1,27 @@
 package org.firstinspires.ftc.teamcode.drive;
 
 
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+import static java.lang.Math.toRadians;
+
 import android.annotation.SuppressLint;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
 
 @TeleOp(name = "Wheels Go Brrr", group = "Linear OpMode")
 public class driveTestForReal extends LinearOpMode {
@@ -18,16 +31,26 @@ public class driveTestForReal extends LinearOpMode {
     private DcMotor rightFrontDriveMotor = null;
     private DcMotor leftRearDriveMotor = null;
     private DcMotor rightRearDriveMotor = null;
+    final double JOYSTICK_DEAD_ZONE = 0.20;
+    final double JOYSTICK_MOVEMENT_SENSITIVITY = 0.75;
+    final double JOYSTICK_ROTATION_SENSITIVITY = 1.00;
+
+    private Servo testServo = null;
+
+    private BNO055IMU imu;
 
     @SuppressLint("DefaultLocale")
     @Override
     public void runOpMode(){
+
 
         //Hardware mapping
        leftFrontDriveMotor = hardwareMap.get(DcMotor.class, "leftFrontDriveMotor");
        rightFrontDriveMotor = hardwareMap.get(DcMotor.class, "rightFrontDriveMotor");
        leftRearDriveMotor = hardwareMap.get(DcMotor.class, "Leftreardrivemotor");
        rightRearDriveMotor = hardwareMap.get(DcMotor.class, "Rightreardrivemotor");
+
+       testServo = hardwareMap.get(Servo.class, "testServo");
 
        //Setting zero power behavior
        leftRearDriveMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -41,50 +64,103 @@ public class driveTestForReal extends LinearOpMode {
         leftRearDriveMotor.setDirection(DcMotor.Direction.FORWARD);
         rightRearDriveMotor.setDirection(DcMotor.Direction.REVERSE);
 
+        waitForStart();
+        runtime.reset();
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample OpMode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+
+        imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
+
         //Telemetry setup to give useful feedback info
 
         //logic to start program
 
-        waitForStart();
-        runtime.reset();
+
+
         //Loop until stop button is pressed
         while (opModeIsActive()){
 
 
-            //TODO: 2. Make a robot centric drive algorithm
             //TODO: 3. Make a field centric drive algorithm with proper usage of formulas
             //TODO: 4. Make a servo respond to an input
             //TODO: 5. Make a servo respond to an input in a more advanced manner
-            double yAxis = -gamepad1.left_stick_y;
-            double xAxis = gamepad1.left_stick_x;
-            double rotX = gamepad1.right_stick_x;
+            double joystickMovementY = inputScaling(-gamepad1.left_stick_y) * JOYSTICK_MOVEMENT_SENSITIVITY;  // Note: pushing stick forward gives negative value
+            double joystickMovementX = inputScaling(gamepad1.left_stick_x) * JOYSTICK_MOVEMENT_SENSITIVITY;
+            double yaw = (inputScaling(gamepad1.right_stick_x) * JOYSTICK_ROTATION_SENSITIVITY) * 0.75;
 
-            double leftFrontPower = yAxis + xAxis + rotX;
-            double rightFrontPower = yAxis - xAxis + rotX;
-            double leftBackPower =  yAxis - xAxis + rotX;
-            double rightBackPower = yAxis + xAxis - rotX;
 
-            double max;
-            max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
-            max = Math.max(max, Math.abs(leftBackPower));
-            max = Math.max(max, Math.abs(rightBackPower));
+            double robotHeading = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
 
-            if (max > 1.0) {
-                leftFrontPower  /= max;
-                rightFrontPower /= max;
-                leftBackPower   /= max;
-                rightBackPower  /= max;
+            double theta = -robotHeading;
+            double movementX = joystickMovementX * cos(toRadians(theta)) - joystickMovementY * sin(toRadians(theta));
+            double movementY = joystickMovementX * sin(toRadians(theta)) + joystickMovementY * cos(toRadians(theta));
+
+
+            double leftFrontPower = (movementY + movementX + yaw);
+            double rightFrontPower = (movementY - movementX - yaw);
+            double leftBackPower = (movementY - movementX + yaw);
+            double rightBackPower = (movementY + movementX - yaw);
+
+
+            double maxPower = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+            maxPower = Math.max(maxPower, Math.abs(leftBackPower));
+            maxPower = Math.max(maxPower, Math.abs(rightBackPower));
+            if (maxPower > 1.0) {
+                leftFrontPower /= maxPower;
+                rightFrontPower /= maxPower;
+                leftBackPower /= maxPower;
+                rightBackPower /= maxPower;
+
             }
-
             leftFrontDriveMotor.setPower(leftFrontPower);
             rightFrontDriveMotor.setPower(rightFrontPower);
             leftRearDriveMotor.setPower(leftBackPower);
             rightRearDriveMotor.setPower(rightBackPower);
 
+            if (gamepad1.b){
+                imu.initialize(parameters);
+            }
+            double servo_clockwise = 1.0;
+            double servo_stop = 0.4;
+            double servo_counterclockwise = 0.0;
 
 
+            if (gamepad2.a){
+                testServo.setPosition(servo_clockwise);
+            }
+            if (gamepad2.x){
+                testServo.setPosition((servo_stop));
+            }
 
+            if (gamepad2.y){
+                testServo.setPosition((servo_counterclockwise));
+
+            }
         }
+    }
+    double inputScaling(double x) {
+        double sign = Math.signum(x);
+        double magnitude = Math.abs(x);
+        if (magnitude < JOYSTICK_DEAD_ZONE) {
+            magnitude = 0.0;
+        } else {
+            magnitude = (magnitude - JOYSTICK_DEAD_ZONE) / (1.0 - JOYSTICK_DEAD_ZONE);
+        }
+        magnitude = Math.pow(magnitude, 2.0);
+        return sign * magnitude;
     }
 
 }
