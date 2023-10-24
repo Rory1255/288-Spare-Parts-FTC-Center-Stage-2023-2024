25b56b1cc69355fbb2d0e23a83287b7020aefd5c
Programming <rorypeyton1255@gmail.com>
2023-10-24 16:09:06 -0500

    made a few adjustments in driveTestForReal.java and added CompetitionReadyTeleOp.java and added theory controls/ghost code- Rory

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/CompetitionReadyTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/CompetitionReadyTeleOp.java
@@ -14,7 +14,6 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.configuration.annotations.ServoType;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -24,15 +23,19 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
 import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
-
-@TeleOp(name = "Wheels Go Brrr", group = "Linear OpMode")
-public class driveTestForReal extends LinearOpMode {
+@TeleOp(name = "Competition Ready TeleOp", group = "Linear OpMode")
+public class CompetitionReadyTeleOp extends LinearOpMode {
     //motor variable declarations
     private final ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftFrontDriveMotor = null;
     private DcMotor rightFrontDriveMotor = null;
     private DcMotor leftRearDriveMotor = null;
     private DcMotor rightRearDriveMotor = null;
+    private DcMotor armExtensionLeft = null;
+    private DcMotor armExtensionRight = null;
+    private DcMotor armHeightMotor = null;
+    private DcMotor extensionEncoder = null;
+    private DcMotor heightEncoder = null;
     //joystick variables
     final double JOYSTICK_DEAD_ZONE = 0.20;
     final double JOYSTICK_MOVEMENT_SENSITIVITY = 0.75;
@@ -40,37 +43,54 @@ public class driveTestForReal extends LinearOpMode {
     //servos
     private CRServo intakeServo = null;
     private Servo hookServo = null;
+    private Servo planeServo = null;
     //imu
     private BNO055IMU imu;
 
     @SuppressLint("DefaultLocale")
     @Override
-    public void runOpMode(){
+    public void runOpMode() {
 
         //Hardware mapping
-       leftFrontDriveMotor = hardwareMap.get(DcMotor.class, "leftFrontDriveMotor");
-       rightFrontDriveMotor = hardwareMap.get(DcMotor.class, "rightFrontDriveMotor");
-       leftRearDriveMotor = hardwareMap.get(DcMotor.class, "Leftreardrivemotor");
-       rightRearDriveMotor = hardwareMap.get(DcMotor.class, "Rightreardrivemotor");
+        leftFrontDriveMotor = hardwareMap.get(DcMotor.class, "leftFrontDriveMotor");
+        rightFrontDriveMotor = hardwareMap.get(DcMotor.class, "rightFrontDriveMotor");
+        leftRearDriveMotor = hardwareMap.get(DcMotor.class, "Leftreardrivemotor");
+        rightRearDriveMotor = hardwareMap.get(DcMotor.class, "Rightreardrivemotor");
 
-       intakeServo = hardwareMap.get(CRServo.class, "intakeServo");
-       hookServo = hardwareMap.get(Servo.class, "hookServo");
+        intakeServo = hardwareMap.get(CRServo.class, "intakeServo");
+        hookServo = hardwareMap.get(Servo.class, "hookServo");
+        planeServo = hardwareMap.get(Servo.class, "planeServo");
 
+        armExtensionLeft = hardwareMap.get(DcMotor.class, "armExtensionLeft");
+        armExtensionRight = hardwareMap.get(DcMotor.class, "armExtensionRight");
+        armHeightMotor = hardwareMap.get(DcMotor.class, "armHeightMotor");
 
-       //Setting zero power behavior
-       leftRearDriveMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-       rightRearDriveMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-       leftFrontDriveMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-       rightFrontDriveMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //Set Run Without Encoders
+        leftFrontDriveMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightFrontDriveMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftRearDriveMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightRearDriveMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-       //Setting motor direction after empirical testing
+        armExtensionLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        armExtensionRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        armHeightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        //Setting zero power behavior
+        leftRearDriveMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightRearDriveMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFrontDriveMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFrontDriveMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        armExtensionLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armExtensionRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armHeightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        //Setting motor direction after empirical testing
         leftFrontDriveMotor.setDirection(DcMotor.Direction.FORWARD);
         rightFrontDriveMotor.setDirection(DcMotor.Direction.REVERSE);
         leftRearDriveMotor.setDirection(DcMotor.Direction.FORWARD);
         rightRearDriveMotor.setDirection(DcMotor.Direction.REVERSE);
 
-        //Run without internal encoders
-        leftFrontDriveMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         //wait for start button to be pressed
         waitForStart();
@@ -78,11 +98,11 @@ public class driveTestForReal extends LinearOpMode {
 
         //set imu parameters
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
-        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
         parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample OpMode
-        parameters.loggingEnabled      = true;
-        parameters.loggingTag          = "IMU";
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
         parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
 
         // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
@@ -98,7 +118,7 @@ public class driveTestForReal extends LinearOpMode {
         telemetry.update();
 
         //Loop until stop button is pressed
-        while (opModeIsActive()){
+        while (opModeIsActive()) {
 
             //scale joystick values and apply sensitivity
             double joystickMovementY = inputScaling(-gamepad1.left_stick_y) * JOYSTICK_MOVEMENT_SENSITIVITY;  // Note: pushing stick forward gives negative value
@@ -150,41 +170,75 @@ public class driveTestForReal extends LinearOpMode {
             rightRearDriveMotor.setPower(rightBackPower);
 
             //reset field centric button
-            if (gamepad1.b){
+            if (gamepad1.y) {
                 imu.initialize(parameters);
             }
+            //TODO: Swap intake servo values if needed after testing
+            //Intake Servo Variables
+            double intakePixel = -1.0;
+            double outtakePixel = 1.0;
+            double stopIntake = 0.0;
 
-            //temporary servo power variables
-            double servo_counterclockwise_fast = -1.0;
-            double servo_counterclockwise_slow = -0.5;
-            double servo_clockwise_fast = 1.0;
-            double servo_clockwise_slow = 0.5;
-            double servo_stop = 0.0;
+            //Pixel Hook Servo Variables
+            double releasePixel = 0.5;
+            double pinPixel = 1.0;
+            double stowHook = 0.0;
 
-            //servo test buttons
-            if (gamepad2.a){
-                intakeServo.setPower(servo_counterclockwise_fast);
+            //Driver Controls
+            //Driver Intake Override
+            if (gamepad1.dpad_down){
+                intakeServo.setPower(intakePixel);
             }
-            if (gamepad2.dpad_down){
-                intakeServo.setPower(intakeServo.getPower()+0.01);
-            }
-            if (gamepad2.dpad_up){
-                intakeServo.setPower(intakeServo.getPower()-0.01);
-            }
-            if (gamepad2.b){
-                intakeServo.setPower(servo_stop);
-            }
-            if (gamepad2.y){
-                intakeServo.setPower(servo_clockwise_fast);
+            if (gamepad1.dpad_up){
+                intakeServo.setPower(outtakePixel);
             }
 
+            //Driver Hook Override
+            if (gamepad1.left_bumper){
+                hookServo.setPosition(releasePixel);
+            }
+            if (gamepad1.right_bumper){
+                hookServo.setPosition(pinPixel);
+            }
+            if (gamepad1.left_bumper && gamepad1.right_bumper){
+                hookServo.setPosition(stowHook);
+            }
 
-
-            telemetry.addData("Servo Power: ", intakeServo.getPower());
+            //Operator intake control
+            if (gamepad2.left_trigger > 0.000){
+                intakeServo.setPower(intakePixel);
+            }
+            if (gamepad2.left_trigger > 0.000 && gamepad2.left_trigger < 0.001){
+                intakeServo.setPower(stopIntake);
+            }
+            //Operator outtake control
+            if (gamepad2.right_trigger > 0.000){
+                intakeServo.setPower(outtakePixel);
+            }
+            if (gamepad2.right_trigger > 0.000 && gamepad2.right_trigger < 0.001){
+                intakeServo.setPower(stopIntake);
+            }
+            double tempFloorposition = 0;
+            //Arm height control
+            if(gamepad2.a) {
+                armHeightMotor.setPower(tempFloorposition);
+            }
+            double releasePlane = 1.0;
+            if(gamepad2.x) {
+                planeServo.setPosition(releasePlane);
+            }
+            if (gamepad2.left_bumper){
+                hookServo.setPosition(releasePixel);
+            }
+            if (gamepad2.right_bumper){
+                hookServo.setPosition(pinPixel);
+            }
+            if (gamepad2.right_bumper && gamepad2.left_bumper){
+                hookServo.setPosition(stowHook);
+            }
             telemetry.update();
         }
     }
-
     //input scaling algorithm
     double inputScaling(double x) {
         double sign = Math.signum(x);
@@ -197,6 +251,4 @@ public class driveTestForReal extends LinearOpMode {
         magnitude = Math.pow(magnitude, 2.0);
         return sign * magnitude;
     }
-
 }
-
