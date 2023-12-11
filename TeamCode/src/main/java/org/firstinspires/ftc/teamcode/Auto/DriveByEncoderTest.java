489a9dd382f5c41bd94c5a2b30c015fb1193b097
Programming <rorypeyton1255@gmail.com>
2023-12-11 16:51:56 -0600

    Drive By encoder but the robot exploded so no programming lol- Rory

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/DriveByEncoderTest.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "Auto Encoder Test", group = "Linear OpMode")
+public class DriveByEncoderTest extends LinearOpMode {
+    /*
+    Current Odo config:
+                        Left Odometry connected to Left Rear Drive Motor
+                        Middle Odometry connected to Right Front Drive Motor
+                        Right Odometry connected to Left Front Drive Motor
+     */
+    public void runOpMode(){
+        /*
+        Brain Dump:
+            ouch
+         */
+    }
+}
